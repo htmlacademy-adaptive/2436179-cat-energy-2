ad956526f5d8f9ef100c2f75440f8e4bfc3311af
Elisaveta Ustiuzhanina <60554440+Tola-lab@users.noreply.github.com>
2024-12-26 14:13:25 +0800

    Добавляет декоративные элементы (#12)
    * Добавляет пустой коммит для пулла
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    *  Добавляет коммит для пулла
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Добавляет декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toggle');
+
+navToggle.addEventListener('click', () => {
+  navMain.classList.toggle('navigation--open');
+});

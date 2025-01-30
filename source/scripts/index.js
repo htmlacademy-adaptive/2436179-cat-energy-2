ebcde29adeab71feb218701eca4d683e822b684c
Elisaveta Ustiuzhanina <60554440+Tola-lab@users.noreply.github.com>
2025-01-30 17:07:34 +0800

    Добавляет правки для допуска на защиту (#15)
    * Добавляет пустой коммит для пулла
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    *  Добавляет коммит для пулла
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Добавляет правки для автоматической проверки
    
    * Merge branch 'master' of https://github.com/Tola-lab/2436179-cat-energy-2
    
    * Добавляет правки для допуска на защиту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,8 @@
 const navMain = document.querySelector('.navigation');
 const navToggle = document.querySelector('.navigation__toggle');
 
+navMain.classList.remove('navigation--nojs');
+
 navToggle.addEventListener('click', () => {
   navMain.classList.toggle('navigation--open');
 });
